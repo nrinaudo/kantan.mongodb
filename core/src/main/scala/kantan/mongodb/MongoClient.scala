edb073d577ebcc79ab42e1b7da307d7343796a86
nrinaudo <nicolas.rinaudo@iosquare.com>
2017-04-10 22:25:49 +0200

    Simplify MongoClient connection.

--- core/src/main/scala/kantan/mongodb/MongoClient.scala
+++ core/src/main/scala/kantan/mongodb/MongoClient.scala
@@ -23,7 +23,10 @@ import kantan.mongodb.MongoClient.DatabaseInfo
 import scala.collection.JavaConverters._
 import scala.util.Try
 
+// TODO: databases() need to be turned into something more generic && safe.
+
 class MongoClient private (private val client: MClient) extends Closeable {
+  /** Returns the database with the specified name. */
   def database(name: String): MongoDatabase = new MongoDatabase(client.getDatabase(name))
 
   def databases(): Iterator[DatabaseInfo] =
@@ -37,34 +40,60 @@ class MongoClient private (private val client: MClient) extends Closeable {
   override def close() = client.close()
 }
 
-@SuppressWarnings(Array("org.wartremover.warts.DefaultArguments"))
+/** Provides functions for connecting to a MongoDB server or cluster.
+  *
+  * There are three main ways to connect to a MongoDB cluster:
+  *
+  *  - the [[local]] one (which basically attempts to connect to [[ServerAddress.default]].
+  *  - a [[fromAddress list of server addresses]].
+  *  - a [[fromUri MongoDB URI]] describing a server cluster.
+  *
+  * The first two come with a `xyzWith` variant that lets you pass in options at connection time.
+  */
 object MongoClient {
   // - Instance creation methods ---------------------------------------------------------------------------------------
   // -------------------------------------------------------------------------------------------------------------------
-  def local(creds: List[MongoCredential] = List.empty,
-            options: Option[MongoClientOptions] = None): MongoClient =
-    fromAddress(List(ServerAddress.default), List.empty, options)
+  private def defaultOptions: MongoClientOptions =
+    com.mongodb.MongoClientOptions.builder().build()
 
+  /** Connects to the local MongoDB server, using the specified credentials.
+    *
+    * Specify an empty list for anonymous login.
+    */
+  def local(creds: List[MongoCredential]): MongoClient =
+    fromAddressWith(List(ServerAddress.default), creds)(defaultOptions)
+
+  /** Connects to the local MongoDB server, using the specified credentials and options.
+    *
+    * Specify an empty list for anonymous login.
+    */
+  def localWith(creds: List[MongoCredential])(options: MongoClientOptions): MongoClient =
+    fromAddressWith(List(ServerAddress.default), creds)(options)
+
+  /** Connects to the specified MongoDB cluster, using the specified credentials.
+    *
+    * Specify an empty list for anonymous login.
+    */
+  def fromAddress(cluster: List[ServerAddress], creds: List[MongoCredential]): MongoClient =
+    fromAddressWith(cluster, creds)(defaultOptions)
+
+  /** Connects to the specified MongoDB cluster, using the specified credentials and options.
+    *
+    * Specify an empty list for anonymous login.
+    */
+  def fromAddressWith(cluster: List[ServerAddress], creds: List[MongoCredential])
+                     (options: MongoClientOptions): MongoClient =
+      new MongoClient(new MClient(cluster.map(_.legacy).asJava, creds.asJava,
+        com.mongodb.MongoClientOptions.builder(options).codecRegistry(kantan.mongodb.io.registry).build(), driverInfo))
+
+  /** Connects to the specified MongoDB cluster. */
   def fromUri(uri: String): Option[MongoClient] =
     Try(new MongoClientURI(uri, com.mongodb.MongoClientOptions.builder().codecRegistry(kantan.mongodb.io.registry)))
       .map(u ⇒ new MongoClient(new MClient(u, driverInfo)))
       .toOption
 
-  def fromAddress(cluster: List[ServerAddress],
-            creds: List[MongoCredential] = List.empty,
-            options: Option[MongoClientOptions] = None): MongoClient = {
-    new MongoClient(new MClient(cluster.map(_.legacy).asJava, creds.asJava, addRegistry(options), driverInfo))
-  }
 
-
-
-  // - Internal helpers ------------------------------------------------------------------------------------------------
-  // -------------------------------------------------------------------------------------------------------------------
-  private def addRegistry(options: Option[MongoClientOptions]): MongoClientOptions =
-    options.map(o ⇒ com.mongodb.MongoClientOptions.builder(o)).getOrElse(com.mongodb.MongoClientOptions.builder())
-      .codecRegistry(kantan.mongodb.io.registry).build()
-
-  def driverInfo: MongoDriverInformation = MongoDriverInformation.builder().driverName("kantan.mongodb")
+  private val driverInfo: MongoDriverInformation = MongoDriverInformation.builder().driverName("kantan.mongodb")
     .driverVersion(BuildInfo.version)
     .driverPlatform("Scala " + scala.util.Properties.scalaPropOrElse("version.number", "unknown")).build()
 
